cca4554a0480e9a298ae9195fccf9e7b76dd8947
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-11-11 01:41:01 +0530

    prac update

--- prac/class4-2/script.js
+++ prac/class4-2/script.js
@@ -1,5 +1,4 @@
-
-
+let globalId = 0
 
 
 
@@ -10,19 +9,49 @@ function addTodo(){
 
     console.log(title);
     console.log(description);
-
+    
     const container = document.getElementById('container');
-    container.innerHTML+=`
-    <div>
-        ${title} <br>
-        ${description} <br> 
-        <button>Mark as done!</button>
-    </div> <br>`
+
+    
+    // ugly methood
+    // container.innerHTML+=`
+    // <div>
+    //     ${title} <br>
+    //     ${description} <br> 
+    //     <button>Mark as done!</button>
+    // </div> <br>`
+
+
+    container.appendChild(createChild(title,description,globalId++ ));
 }
 
+// .createElement .appendChild globalId .setAttribute 
 
+function createChild(title, description,id) {
+    const child = document.createElement('div');
 
+    const firstGrandChild = document.createElement('div');
+    firstGrandChild.innerHTML = title;
 
+    const secondGrandChild = document.createElement('div');
+    secondGrandChild.innerHTML = description;
+
+    const thirdGrandChild = document.createElement('button');
+    thirdGrandChild.innerHTML = 'Mark as Done!';
+
+    child.appendChild(firstGrandChild);
+    child.appendChild(secondGrandChild);
+    child.appendChild(thirdGrandChild);
+
+    thirdGrandChild.setAttribute('onclick',`markAsDone(${id})`);
+
+    return child;
+}
+
+function markAsDone(todoId) {
+    const container = document.getElementById(todoId);
+    container.children[2].innerHTML = 'Done!';
+}
 
 
 
